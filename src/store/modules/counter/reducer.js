2467b63316452e8a48ac353ea90d137ec4ab4a0b
glen1986 <escobar.glen@gmail.com>
2021-09-28 19:21:42 -0300

    action

--- src/store/modules/counter/reducer.js
+++ src/store/modules/counter/reducer.js
@@ -1,11 +1,10 @@
-import {actionTypes} from 'redux-resource';
-import {ADD_NUM} from "./actionTypes";
-import {LES_NUM} from "./actionTypes";
 const reducerCounter = (state = 0, action) => {
+  console.log(action)
   switch (action.type) {
     case 'ADD_NUM':
+      console.log("mas")
       return state + action.payload;
-    case "SUB_NUM":
+    case "LES_NUM":
       return state - action.payload;
     default:
       return state;
